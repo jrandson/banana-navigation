bd2d5899f6d5a0982fbd866338ee7c02a8b2a550
randson <randson@127.0.0.1localhost>
2019-04-04 06:52:59 -0300

    adding git ignore

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ from DQLearning.dqn_agent import Agent
 
 
 
+
 def run_sarsa():
+
     # please do not modify the line below
     env = UnityEnvironment(file_name="./Banana_Linux/Banana.x86_64")
     brain_name = env.brain_names[0]
@@ -27,6 +29,45 @@ def run_sarsa():
 
     scores = run(q_agent, env)
 
+
+def dqn(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []  # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start  # initialize epsilon
+    for i_episode in range(1, n_episodes + 1):
+        state = env.reset()
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state, eps)
+            next_state, reward, done, _ = env.step(action)
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break
+        scores_window.append(score)  # save most recent score
+        scores.append(score)  # save most recent score
+        eps = max(eps_end, eps_decay * eps)  # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window) >= 200.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - 100,
+                                                                                         np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+            break
+
+    return scores
 
 if __name__=="__main__":
 
@@ -53,9 +94,9 @@ if __name__=="__main__":
 
     agent = Agent(state_size=state_size, action_size=action_size, seed=0)
 
-    env.seed(0)
-    print('State shape: ', env.observation_space.shape)
-    print('Number of actions: ', env.action_space.n)
+    # env.seed(0)
+    # print('State shape: ', env.observation_space.shape)
+    # print('Number of actions: ', env.action_space.n)
 
     scores = dqn()
 
